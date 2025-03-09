6e1edb9553deec5e605e6a1f2d75111e2e06261e
OlgaSm33 <olya.sudakova98@gmail.com>
2025-03-09 17:23:34 +0700

    Исправлены ошибки

--- Practices.Tests/PracticesTests.cs
+++ Practices.Tests/PracticesTests.cs
@@ -21,7 +21,7 @@ namespace Practices.Tests
         [Test]
         public void Additional_ValidInputs_ReturnsCorrectValue(int x, int y, int expected)
         {
-            Assert.That(calculator.Additional(7, 5), Is.EqualTo(12));
+            Assert.That(calculator.Additional(x, y), Is.EqualTo(expected));
 
         }
         #endregion
@@ -34,33 +34,33 @@ namespace Practices.Tests
         [Test]
         public void Subtraction_ValidInputs_ReturnsCorrectValue(int x, int y, int expected)
         {
-            Assert.That(calculator.Subtraction(7, 5), Is.EqualTo(2));
+            Assert.That(calculator.Subtraction(x, y), Is.EqualTo(expected));
 
         }
         #endregion
 
         #region Multiplication Tests
-        [TestCase(7, 5, 2)]
-        [TestCase(12, -5, 17)]
-        [TestCase(-3, 7, -10)]
-        [TestCase(-12, -5, -7)]
-        [Test]
-        public void Multiplication_ValidInputs_ReturnsCorrectValue(int x, int y, int expected)
-        {
-            Assert.That(calculator.Multiplication(7, 5), Is.EqualTo(35));
-
-        }
-        #endregion
-
-        #region Division Tests
         [TestCase(7, 5, 35)]
         [TestCase(12, -5, -60)]
         [TestCase(-12, -5, 60)]
         [TestCase(0, 25, 0)]
         [Test]
+        public void Multiplication_ValidInputs_ReturnsCorrectValue(int x, int y, int expected)
+        {
+            Assert.That(calculator.Multiplication(x, y), Is.EqualTo(expected));
+
+        }
+        #endregion
+
+        #region Division Tests
+        [TestCase(8, 3, 2)]
+        [TestCase(8, -3, -2)]
+        [TestCase(-8, -3, 2)]
+        [TestCase(0, 25, 0)]
+        [Test]
         public void Division_ValidInputs_ReturnsCorrectValue(int x, int y, int expected)
         {
-            Assert.That(calculator.Division(8, 3), Is.EqualTo(2));
+            Assert.That(calculator.Division(x, y), Is.EqualTo(expected));
 
         }
 
