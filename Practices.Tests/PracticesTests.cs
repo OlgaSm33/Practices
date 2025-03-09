f068750c1c5c24d3f1dac29ea4394569b1aa1f97
OlgaSm33 <olya.sudakova98@gmail.com>
2025-03-09 17:17:26 +0700

    Добавлены тест кейсы, что делает код более читаемым.

--- Practices.Tests/PracticesTests.cs
+++ Practices.Tests/PracticesTests.cs
@@ -1,139 +1,70 @@
 namespace Practices.Tests
 {
+    [TestFixture]
     public class PracticesTests
     {
-        /// <summary>
-        /// тесты для Additional
-        /// </summary>
+
+        private Calculator calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            calculator = new Calculator();
+        }
+
+
+        #region Additional Tests
+        [TestCase(7, 5, 12)]
+        [TestCase(12, -5, 7)]
+        [TestCase(-12, -5, -17)]
+        [TestCase(0, 25, 25)]
         [Test]
-        public void Additional_TwoPositiveNumbers_ReturnsCorrectValue()
+        public void Additional_ValidInputs_ReturnsCorrectValue(int x, int y, int expected)
         {
             var calculator = new Calculator();
             Assert.That(calculator.Additional(7, 5), Is.EqualTo(12));
 
         }
+        #endregion
 
+        #region Subtraction Tests
+        [TestCase(7, 5, 2)]
+        [TestCase(12, -5, 17)]
+        [TestCase(-3, 7, -10)]
+        [TestCase(-12, -5, -7)]
         [Test]
-        public void Additional_PositiveAndNegativeNumbers_ReturnsCorrectValue()
-        {
-            var calculator = new Calculator();
-            Assert.That(calculator.Additional(12, -5), Is.EqualTo(7));
-
-        }
-
-        [Test]
-        public void Additional_TwoNegativeNumbers_ReturnsCorrectValue()
-        {
-            var calculator = new Calculator();
-            Assert.That(calculator.Additional(-12, -5), Is.EqualTo(-17));
-
-        }
-
-
-
-
-
-
-
-        /// <summary>
-        /// тесты для Subtraction
-        /// </summary>
-        [Test]
-        public void Subtraction_TwoPositiveNumbers_ReturnsCorrectValue()
+        public void Subtraction_ValidInputs_ReturnsCorrectValue(int x, int y, int expected)
         {
             var calculator = new Calculator();
             Assert.That(calculator.Subtraction(7, 5), Is.EqualTo(2));
 
         }
+        #endregion
 
+        #region Multiplication Tests
+        [TestCase(7, 5, 2)]
+        [TestCase(12, -5, 17)]
+        [TestCase(-3, 7, -10)]
+        [TestCase(-12, -5, -7)]
         [Test]
-        public void Subtraction_PositiveAndNegativeNumbers_ReturnsCorrectValue()
-        {
-            var calculator = new Calculator();
-            Assert.That(calculator.Subtraction(12, -5), Is.EqualTo(17));
-
-        }
-
-        [Test]
-        public void Subtraction_NegativeAndPositiveNumbers_ReturnsCorrectValue()
-        {
-            var calculator = new Calculator();
-            Assert.That(calculator.Subtraction(-3, 7), Is.EqualTo(-10));
-
-        }
-
-        [Test]
-        public void Subtraction_TwoNegativeNumbers_ReturnsCorrectValue()
-        {
-            var calculator = new Calculator();
-            Assert.That(calculator.Subtraction(-12, -5), Is.EqualTo(-7));
-
-        }
-
-
-
-
-
-
-
-
-        /// <summary>
-        /// тесты для Multiplication
-        /// </summary>
-        [Test]
-        public void Multiplication_TwoPositiveNumbers_ReturnsCorrectValue()
+        public void Multiplication_ValidInputs_ReturnsCorrectValue(int x, int y, int expected)
         {
             var calculator = new Calculator();
             Assert.That(calculator.Multiplication(7, 5), Is.EqualTo(35));
 
         }
+        #endregion
 
+        #region Division Tests
+        [TestCase(7, 5, 35)]
+        [TestCase(12, -5, -60)]
+        [TestCase(-12, -5, 60)]
+        [TestCase(0, 25, 0)]
         [Test]
-        public void Multiplication_PositiveAndNegativeNumbers_ReturnsCorrectValue()
-        {
-            var calculator = new Calculator();
-            Assert.That(calculator.Multiplication(12, -5), Is.EqualTo(-60));
-
-        }
-
-
-        [Test]
-        public void Multiplication_TwoNegativeNumbers_ReturnsCorrectValue()
-        {
-            var calculator = new Calculator();
-            Assert.That(calculator.Multiplication(-12, -5), Is.EqualTo(60));
-
-        }
-
-
-
-
-        /// <summary>
-        /// тесты для Division
-        /// </summary>
-
-        [Test]
-        public void Division_TwoPositiveNumbers_ReturnsCorrectValue()
+        public void Division_ValidInputs_ReturnsCorrectValue(int x, int y, int expected)
         {
             var calculator = new Calculator();
             Assert.That(calculator.Division(8, 3), Is.EqualTo(2));
-
-        }
-
-        [Test]
-        public void Division_PositiveAndNegativeNumbers_ReturnsCorrectValue()
-        {
-            var calculator = new Calculator();
-            Assert.That(calculator.Division(8, -3), Is.EqualTo(-2));
-
-        }
-
-
-        [Test]
-        public void Division_TwoNegativeNumbers_ReturnsCorrectValue()
-        {
-            var calculator = new Calculator();
-            Assert.That(calculator.Division(-8, -3), Is.EqualTo(2));
 
         }
 
@@ -147,7 +78,7 @@ namespace Practices.Tests
            
 
         }
-
+        #endregion
 
     }
 }
