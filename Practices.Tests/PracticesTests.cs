2f4f4873b08d14f799bb6cdbbbb0fb45cf5e6cfd
OlgaSm33 <olya.sudakova98@gmail.com>
2025-03-09 17:24:45 +0700

    И ещё исправлены ошибки

--- Practices.Tests/PracticesTests.cs
+++ Practices.Tests/PracticesTests.cs
@@ -18,7 +18,6 @@ namespace Practices.Tests
         [TestCase(12, -5, 7)]
         [TestCase(-12, -5, -17)]
         [TestCase(0, 25, 25)]
-        [Test]
         public void Additional_ValidInputs_ReturnsCorrectValue(int x, int y, int expected)
         {
             Assert.That(calculator.Additional(x, y), Is.EqualTo(expected));
@@ -31,7 +30,6 @@ namespace Practices.Tests
         [TestCase(12, -5, 17)]
         [TestCase(-3, 7, -10)]
         [TestCase(-12, -5, -7)]
-        [Test]
         public void Subtraction_ValidInputs_ReturnsCorrectValue(int x, int y, int expected)
         {
             Assert.That(calculator.Subtraction(x, y), Is.EqualTo(expected));
@@ -44,7 +42,6 @@ namespace Practices.Tests
         [TestCase(12, -5, -60)]
         [TestCase(-12, -5, 60)]
         [TestCase(0, 25, 0)]
-        [Test]
         public void Multiplication_ValidInputs_ReturnsCorrectValue(int x, int y, int expected)
         {
             Assert.That(calculator.Multiplication(x, y), Is.EqualTo(expected));
@@ -57,7 +54,6 @@ namespace Practices.Tests
         [TestCase(8, -3, -2)]
         [TestCase(-8, -3, 2)]
         [TestCase(0, 25, 0)]
-        [Test]
         public void Division_ValidInputs_ReturnsCorrectValue(int x, int y, int expected)
         {
             Assert.That(calculator.Division(x, y), Is.EqualTo(expected));
